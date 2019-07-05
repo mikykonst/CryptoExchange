62887c20fd55a96eb018ad08e19d5f4abdf4825d
mkons <mikykonst66@gmail.com>
2019-07-05 10:49:51 +0300

    Charts was added

---
+++ src/app/components/crypto-chart/crypto-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-chart',
+  templateUrl: './crypto-chart.component.html',
+  styleUrls: ['./crypto-chart.component.scss']
+})
+export class CryptoChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
